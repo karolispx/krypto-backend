fd2dbbeece6fde2e962ad7913f63f841903555a1
Karolis Pliauskys <k.pliauskys@gmail.com>
2022-02-10 12:40:08 +0000

    User API endpoints

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -24,7 +24,180 @@ const Users = {
         return Boom.notFound("internal db failure");
       }
     },
-  }
+  },
+
+  register: {
+    auth: false,
+    validate: {
+      payload: {
+        email: Joi.string().email().required(),
+        password: Joi.string().required(),
+        repeatpassword: Joi.string().required(),
+      },
+      failAction: function (request, h, error) {
+        console.log(error.details)
+        return Boom.badData(error.details);
+      },
+    },
+    handler: async function (request, h) {
+      try {
+        const payload = request.payload;
+
+        if (payload.password !== payload.repeatpassword) {
+          return Boom.badData("Passwords do not match");
+        }
+
+        let user = await User.findByEmail(payload.email);
+
+        if (user) {
+          return Boom.badData("Email address is already registered");
+        }
+
+        const newUser = new User({
+          email: payload.email,
+          password: await bcrypt.hash(payload.password, 10)
+        });
+
+        user = await newUser.save();
+
+        console.log(user)
+        return h.response(user).code(201);
+      } catch (err) {
+        return Boom.badData(err.message);
+      }
+    },
+  },
+
+  passwordReset: {
+    auth: false,
+    validate: {
+      payload: {
+        email: Joi.string().email().required()
+      },
+      failAction: function (request, h, error) {
+        console.log(error.details)
+        return Boom.badData(error.details);
+      },
+    },
+    handler: async function (request, h) {
+      try {
+        const payload = request.payload;
+  
+        let user = await User.findByEmail(payload.email);
+
+        if (user) {
+          // User exists, send email
+
+          // TODO: Send email with password reset
+        }
+
+        // User does not exists, no email sent but do not notify user if account doesn't exist
+
+        return h.response({ success: true, message: "Email has been sent out with password reset instructions if this email exists." }).code(200);
+      } catch (err) {
+        return Boom.badData(err.message);
+      }
+    },
+  },
+
+  authenticatedUser: {
+    auth: {
+      strategy: "jwt",
+    },
+    handler: async function (request, h) {
+      const userId = request.auth.credentials.id;
+
+      if (!userId) {
+        return Boom.unauthorized("Not logged in");
+      }
+
+      const user = await User.findOne({ _id: userId });
+
+      return h.response(user).code(200);
+    },
+  },
+
+  updateEmail: {
+    auth: {
+      strategy: "jwt",
+    },
+    validate: {
+      payload: {
+        email: Joi.string().email().required()
+      },
+      failAction: function (request, h, error) {
+        console.log(error.details)
+        return Boom.badData(error.details);
+      },
+    },
+    handler: async function (request, h) {
+      const userId = request.auth.credentials.id;
+
+      if (!userId) {
+        return Boom.unauthorized("Not logged in");
+      }
+
+      try {
+        const userEdit = request.payload;
+  
+        const user = await User.findById(userId);
+
+        // Validate email address
+        if (await User.findOne({email: userEdit.email, _id: {$ne: userId} }).lean()) {          
+          return Boom.badData("Email address is already registered");
+        }
+
+        user.email = userEdit.email;
+
+        await user.save();
+
+        return h.response(user).code(200);
+      } catch (err) {
+        return Boom.badData(err.message);
+      }
+    },
+  },
+
+  updatePassword: {
+    auth: {
+      strategy: "jwt",
+    },
+    validate: {
+      payload: {
+        password: Joi.string().required(),
+        repeatpassword: Joi.string().required()
+      },
+      failAction: function (request, h, error) {
+        console.log(error.details)
+        return Boom.badData(error.details);
+      },
+    },
+    handler: async function (request, h) {
+      const userId = request.auth.credentials.id;
+
+      if (!userId) {
+        return Boom.unauthorized("Not logged in");
+      }
+
+      try {
+        const userEdit = request.payload;
+  
+        const user = await User.findById(userId);
+
+        if (userEdit.password !== userEdit.repeatpassword) {
+          return Boom.badData("Passwords do not match");
+        }
+
+        user.password = await bcrypt.hash(userEdit.password, 10);;
+
+        await user.save();
+
+        return h.response(user).code(200);
+      } catch (err) {
+        return Boom.badData(err.message);
+      }
+    },
+  },
 };
 
 module.exports = Users;
--- routes.js
+++ routes.js
@@ -3,6 +3,12 @@
 const Users = require("./app/controllers/users");
 
 module.exports = [
-  { method: 'POST', path: '/api/users/authenticate', config: Users.authenticate }
+  // User endpoints
+  { method: 'POST', path: '/api/users/authenticate', config: Users.authenticate },
+  { method: 'POST', path: '/api/users', config: Users.register },
+  { method: 'POST', path: '/api/password-reset', config: Users.passwordReset },
+  { method: 'GET', path: '/api/users/me', config: Users.authenticatedUser },
+  { method: 'POST', path: '/api/users/me/email', config: Users.updateEmail },
+  { method: 'POST', path: '/api/users/me/password', config: Users.updatePassword },
 ];
 
